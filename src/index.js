4132965d8996a0cc31809a9410002a01954f1464
Davvo1 <davidebonardi15@gmail.com>
2023-05-25 16:23:02 +0100

    Project finished
    finished all the main parts, now adding the last features

--- src/index.js
+++ src/index.js
@@ -1,30 +1,62 @@
 const submit = document.querySelector("#submit");
 const bookList = document.querySelector("#book-list");
 submit.addEventListener("click", submitForm);
+let key;
+const popUp = document.querySelector("#popup");
+const bookDescription = document.querySelector("#book-desc");
+
 
 function submitForm(e) {
-    const bookGenre = document.querySelector("#input").value;
-    console.log(bookGenre);
+    const bookGenre = document.querySelector("#input").value.toLowerCase();
     e.preventDefault();
         async function contactApi() {
             let response = await fetch(`https://openlibrary.org/subjects/${bookGenre}.json`);
-            let json = await response.json();
-            let obj = JSON.stringify(json);
-            let obj2 = JSON.parse(obj)
-            console.log(obj2.works);
+            let obj2 = await response.json();
+            bookList.innerHTML = '';
             for (let i = 0; i < obj2.works.length; i++) {
-                console.log(obj2.works[i]);
-                
+                key = obj2.works[i].key;
                 const bookItem = document.createElement("div");               
                 bookItem.innerHTML = `
-                <img class="image-item" src="/src/read.ico">
-                <span>${obj2.works[i].title}</span>`
+                <img class="image-item" src="https://covers.openlibrary.org/b/id/${obj2.works[i].cover_id}-M.jpg?default=false">
+                <span class="span-item" id="my-span">${obj2.works[i].title}</span>
+                <span class="author">${obj2.works[i].authors[0].name}</span>`
                 bookItem.classList.add("container-item");
                 bookList.append(bookItem);
             }
-            
 
+
+            return obj2.works;
         }
-    contactApi();
-  } 
+    
+        contactApi().then(works => {
+            anotherFunction(works);
+        });
+    }
+function anotherFunction(works) {
+       let keys = works.map(work => work.key);
+        const spanItems = document.querySelectorAll(".span-item");
+        for(let i = 0; i < works.length; i++) {
+            const mySpan = spanItems[i];
+            const key = keys[i];
 
+            mySpan.addEventListener("click", () => {
+                fetchUrl(key);
+            });
+        }
+    }
+
+    async function fetchUrl(key) {
+        let response = await fetch(`https://openlibrary.org${key}.json`);
+        let json = await response.json();
+        console.log(json.description.value || json.description);
+        bookDescription.innerHTML = `
+        <img src="/src/x-icon.png" class="close" onclick="closePopUp()">
+        <p>${json.description.value || json.description}</p>`
+        popUp.className = "popup";
+
+    }
+
+    function closePopUp() {
+        bookDescription.innerHTML = '';
+        popUp.classList.remove("popup");
+    }
